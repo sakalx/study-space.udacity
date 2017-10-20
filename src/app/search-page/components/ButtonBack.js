6a497f161ed5ef1c6302e962be3a8d9763f9f9c4
sakals <sakals@mail.ua>
2017-10-19 22:03:33 -0500

    fix: increase size loader
    Signed-off-by: sakals <sakals@mail.ua>

--- src/app/search-page/components/ButtonBack.js
+++ src/app/search-page/components/ButtonBack.js
@@ -5,14 +5,14 @@ import IconButton from 'material-ui/IconButton';
 import ActionHome from 'material-ui/svg-icons/action/home';
 
 const styles = {
-  mediumIcon: {
-    width: 48,
-    height: 48,
+  largeIcon: {
+    width: 60,
+    height: 60,
   },
-  medium: {
-    width: 96,
-    height: 96,
-    padding: 24,
+  large: {
+    width: 120,
+    height: 120,
+    padding: 30,
   },
 };
 
@@ -21,10 +21,9 @@ const BtnAdd = props =>
       <IconButton
           tooltip="Back to MyReads" touch={true}
           tooltipPosition="bottom-right"
-          iconStyle={styles.mediumIcon}
-          style={styles.medium}
-      >
-        <ActionHome />
+          iconStyle={styles.largeIcon}
+          style={styles.large}>
+        <ActionHome/>
       </IconButton>
     </Link>;
 
