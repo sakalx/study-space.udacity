9940e74a3b518ab9d904b03876d3d981e991ea13
sakals <sakals@mail.ua>
2017-10-30 19:37:34 -0500

    init: router lives here
    Signed-off-by: sakals <sakals@mail.ua>

--- contacts.app/src/app/App.js
+++ contacts.app/src/app/App.js
@@ -2,19 +2,16 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 
 import HomePage from './home-page/Index';
+import NewUserPage from './new-user-page/Index';
 import NotFound from './404/Index';
 
-class App extends React.Component {
-  render() {
-    return (
-        <div>
-          <Switch>
-            <Route exact path='/' component={HomePage}/>
-            <Route component={NotFound}/>
-          </Switch>
-        </div>
-    );
-  }
-}
+const App = () =>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/newContact' component={NewUserPage}/>
+        <Route component={NotFound}/>
+      </Switch>
+    </div>;
 
 export default App;
