ef4b81314c06a998e4f20bc156a0497c4bdc5111
sakals <sakals@mail.ua>
2017-10-30 19:39:26 -0500

    init: main page
    Signed-off-by: sakals <sakals@mail.ua>

--- contacts.app/src/app/home-page/Index.js
+++ contacts.app/src/app/home-page/Index.js
@@ -2,35 +2,65 @@ import React from 'react';
 import * as callData from 'root/api/LocalStorage';
 
 import TextField from 'material-ui/TextField';
+import AddBtn from './components/AddBtn';
+import FlatButton from 'material-ui/FlatButton';
 import UserCard from '../user-card/Index';
 import TearSheet from './components/TearSheet';
+import SnackBar from '../snack-bar/Index';
 
 class HomePage extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {contacts: []};
+
+  state = {
+    contacts: [],
+    query: '',
+    openBar: false,
+    messageBar: '',
+  };
+
+  componentDidMount() {
+    this.setState({contacts: callData.getStorage()});
   }
 
-  componentWillMount() {
-    this.setState({contacts: callData.lStorage()});
-  }
+  handleQuery = e => {
+    this.setState({query: e.target.value.trim()});
+  };
 
-  handleRemove = id => this.setState({contacts: callData.remove(id)});
+  handleShowAll = () => {
+    this.setState({query: ''});
+  };
+
+  handleRemove = id => {
+    this.setState({
+      contacts: callData.remove(id),
+      openBar: true,
+      messageBar: `Contact was Successfully Removed`,
+    });
+  };
 
   render() {
-    console.log(this.state.contacts);
+    const {contacts, query, messageBar, openBar} = this.state;
+
+    const showingContacts = (query) ? contacts.filter(obj =>
+        new RegExp('\\b' + query, 'gi').test(obj.name)) : contacts;
+
     return (
         <main>
-          <h1>Contacts</h1>
+          <h1>myContacts</h1>
+          <AddBtn/>
           <TextField
+              onChange={this.handleQuery}
               hintText="What contact are you looking"
               floatingLabelText="Search..."
-              fullWidth={true}
-          />
+              fullWidth={true}/>
+          {query &&
+          <div style={{textAlign: 'center'}}>
+            <span>Now Showing {showingContacts.length} of {contacts.length} total</span>
+            <FlatButton onClick={this.handleShowAll} label="Show all" secondary={true}/>
+          </div>}
           <TearSheet>
-            <UserCard contacts={this.state.contacts}
-                      remove={this.handleRemove.bind(this)}/>
+            <UserCard contacts={showingContacts} remove={this.handleRemove}/>
           </TearSheet>
+          <SnackBar message={messageBar} open={openBar}/>
         </main>
     );
   }
