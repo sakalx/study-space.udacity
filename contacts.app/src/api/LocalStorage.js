e7e3e2f9216eb7c9452c09f92544fc0194f327f9
sakals <sakals@mail.ua>
2017-10-30 19:45:12 -0500

    init: handle localStorage
    Signed-off-by: sakals <sakals@mail.ua>

--- contacts.app/src/api/LocalStorage.js
+++ contacts.app/src/api/LocalStorage.js
@@ -1,40 +1,39 @@
 // Keeping data in local Storage instead server
-export const lStorage = () => {
+export const getStorage = () => {
   let data = localStorage.data;
   if (!data)
-    data = localStorage.setItem('data', JSON.stringify(
-        [
-          {
-            'id': 'ryan',
-            'name': 'Ryan Florence',
-            'email': 'ryan@reacttraining.com',
-            'avatarURL': 'http://localhost:5001/ryan.jpg',
-          },
-          {
-            'id': 'michael',
-            'name': 'Michael Jackson',
-            'email': 'michael@reacttraining.com',
-            'avatarURL': 'http://localhost:5001/michael.jpg',
-          },
-          {
-            'id': 'tyler',
-            'name': 'Tyler McGinnis',
-            'email': 'tyler@reacttraining.com',
-            'avatarURL': 'http://localhost:5001/tyler.jpg',
-          },
-        ],
-    ));
+    data = localStorage.setItem('data', JSON.stringify([
+      {
+        'id': 'uniqueID:)',
+        'name': 'Fang Yuan 🤤',
+        'email': 'yuan2015@mail.ru',
+        'img': 'https://sakals.000webhostapp.com/share/HelloKitty5.png',
+      },
+      {
+        'id': 'uniqueID1:)',
+        'name': 'Gao Fushuai 😋',
+        'email': 'gao.fush91@gmai.com',
+        'img': 'https://sakals.000webhostapp.com/share/HelloKitty2.png',
+      },
+      {
+        'id': 'uniqueID2:)',
+        'name': 'Fan Mimi 😁',
+        'email': 'mimitwo@hotmail.com',
+        'img': 'https://sakals.000webhostapp.com/share/HelloKitty3.png',
+      },
+    ]));
   return JSON.parse(localStorage.getItem('data'));
 };
 
 export const remove = id => {
-  const newData = lStorage().filter(contact => contact.id !== id);
+  const newData = getStorage().filter(contact => contact.id !== id);
   localStorage.setItem('data', JSON.stringify(newData));
   return newData;
 };
 
 export const create = obj => {
-  const newData = lStorage();
+  obj.id = +new Date();
+  const newData = getStorage();
   newData.push(obj);
   localStorage.setItem('data', JSON.stringify(newData));
 };
