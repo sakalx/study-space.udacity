d41666c5665df18d3925460dab6b12a8d290e275
sakals <sakals@mail.ua>
2017-11-17 21:17:21 -0600

    init: add initialState post
    Signed-off-by: sakals <sakals@mail.ua>

---
+++ readable.app/app/reducers/initialStates.js
@@ -0,0 +1,17 @@
+const initialState = {
+  post: {
+    'default': {
+      id: null,
+      timestamp: null,
+      title: null,
+      body: null,
+      author: null,
+      category: null,
+      voteScore: null,
+      deleted: false,
+      commentCount: null,
+    },
+  },
+};
+
+export default initialState;
