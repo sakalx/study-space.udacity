42aec573ce22df3eff18c7dcb09e92520d83cb9b
sakals <sakals@mail.ua>
2017-11-22 22:03:49 -0600

    feat: handleVotePost handleDisable
    Signed-off-by: sakals <sakals@mail.ua>

--- readable.app/app/card-post/PostActions.js
+++ readable.app/app/card-post/PostActions.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {disablePost, votePost} from 'root/app/actions/post';
 
 import {CardActions} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
@@ -7,19 +10,38 @@ import ActionEdit from 'material-ui/svg-icons/editor/mode-edit';
 import ActionVoteUp from 'material-ui/svg-icons/action/thumb-up';
 import ActionVoteDown from 'material-ui/svg-icons/action/thumb-down';
 
+@connect(store => ({posts: store.post}))
+
 class PostActions extends React.Component {
 
+  postId = this.props.postId;
+  dispatch = this.props.dispatch;
+
+  handleDisable = () =>
+      this.dispatch(disablePost(this.postId));
+
+  handleVotePost = vote =>
+      this.dispatch(votePost(this.postId, vote));
+
   render() {
+    const {voteUp, voteDown} = this.props.posts[this.postId];
+
     return (
         <CardActions>
-          <FlatButton label="1222" icon={<ActionVoteUp/>}/>
-          <FlatButton label="-22" icon={<ActionVoteDown/>}/>
+          <FlatButton label={voteUp} onClick={this.handleVotePost.bind(this, 1)}
+                      icon={<ActionVoteUp/>}/>
+          <FlatButton label={voteDown} onClick={this.handleVotePost.bind(this, -1)}
+                      icon={<ActionVoteDown/>}/>
           <FlatButton label="Edit post" icon={<ActionEdit/>}/>
-          <FlatButton label="Delete post" icon={<ActionDelete/>} secondary={true}/>
+          <FlatButton label="Delete post" onClick={this.handleDisable}
+                      icon={<ActionDelete/>} secondary={true}/>
           <FlatButton label="Read more" primary={true}/>
         </CardActions>
     );
   }
 }
+PostActions.propTypes = {
+  postId: PropTypes.string,
+};
 
 export default PostActions;
