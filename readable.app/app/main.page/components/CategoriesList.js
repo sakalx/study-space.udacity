3556e85e3307d4f28a53e37caf16d7fa2d0fba0d
sakals <sakals@mail.ua>
2017-11-25 20:57:08 -0600

    init: showing categories
    Signed-off-by: sakals <sakals@mail.ua>

---
+++ readable.app/app/main.page/components/CategoriesList.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {getAllCategory, toggleCategory} from 'root/app/actions/category';
+
+import {List, ListItem} from 'material-ui/List';
+import Toggle from 'material-ui/Toggle';
+
+@connect(store => ({
+  categories: store.categories,
+}))
+
+class CategoriesList extends React.Component {
+  dispatch = this.props.dispatch;
+
+  componentDidMount = () => {
+    this.dispatch(getAllCategory());
+  };
+
+  handleToggle = name => {
+    this.dispatch(toggleCategory(name));
+  };
+
+  render() {
+    const {categories} = this.props;
+    const categoriesKey = Object.keys(categories);
+
+    return (
+        <List>
+          {categoriesKey.map(id =>
+              <ListItem key={id}
+                        primaryText={categories[id].name}
+                        rightToggle={<Toggle toggled={categories[id].active}
+                                             onToggle={() => this.handleToggle(id)}
+                        />}
+              />)
+          }
+        </List>
+    );
+  }
+}
+
+export default CategoriesList;
