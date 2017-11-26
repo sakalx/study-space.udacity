56a214360cf76efec4204e2467ba883063a70d05
sakals <sakals@mail.ua>
2017-11-25 20:55:53 -0600

    feat: + category
    Signed-off-by: sakals <sakals@mail.ua>

--- readable.app/app/actions/actionTypes.js
+++ readable.app/app/actions/actionTypes.js
@@ -1,10 +1,14 @@
 const actionTypes = {
   post: {
-    ALL: 'GET_ALL',
+    ALL: 'GET_ALL_POSTS',
     ADD: 'ADD_POST',
     VOTE: 'VOTE_POST',
     DISABLE: 'DISABLE_POST',
     EDIT: 'EDIT_POST',
+  },
+  category: {
+    ALL: 'GET_ALL_CATEGORIES',
+    TOGGLE: 'TOGGLE_CATEGORY',
   },
 };
 
