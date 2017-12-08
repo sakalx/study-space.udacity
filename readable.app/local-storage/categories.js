3544a840cb7d386f58cead7874cdf66b34808194
sakals <sakals@mail.ua>
2017-12-07 23:08:26 -0600

    feat: handel actions categories
    Signed-off-by: sakals <sakals@mail.ua>

--- readable.app/local-storage/categories.js
+++ readable.app/local-storage/categories.js
@@ -4,7 +4,52 @@ import {defaultCategories} from './default-data';
 const setData = data => lStorage.setData('categories', data);
 const getData = () => lStorage.getData('categories', defaultCategories);
 
-export const getAllCategories = () => getData();
+export const getAllCategories = () => {
+  const categories = getData();
+  let existingCategories = {};
 
+  for (let key in categories) {
+    if (!categories[key].deleted) {
+      existingCategories[categories[key].id] = categories[key];
+    }
+  }
+  return existingCategories;
+};
 
+export const addCategory = (
+    name = null,
+    id = (+new Date()).toString(36),) => {
+  const categories = getData();
+
+  categories[id] = {
+    id,
+    name,
+    active: false,
+    deleted: false,
+  };
+  setData(categories);
+
+  return categories[id];
+};
+
+export const disableCategories = ids => {
+  const categories = getData();
+
+  ids.forEach(id => {
+    categories[id].deleted = true;
+    categories[id].active = false;
+  });
+  setData(categories);
+
+  return categories;
+};
+
+export const editCategory = (id, name) => {
+  const categories = getData();
+
+  categories[id].name = name;
+  setData(categories);
+
+  return categories[id];
+};
 
