c739c41b8a97f452986eca4db895c409a6463cea
sakals <sakals@mail.ua>
2017-12-07 23:11:10 -0600

    fix: bug withRouter
    Signed-off-by: sakals <sakals@mail.ua>

--- readable.app/app/cards/post/Post.js
+++ readable.app/app/cards/post/Post.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
+import {Link, withRouter} from 'react-router-dom';
+import muiTheme from 'root/theme';
 
 import {Card, CardActions, CardHeader, CardText, CardTitle} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
-import VoteAction from '../client-actions/vote/VoteAction';
-import DeleteEditAction from '../client-actions/delete-edit/DeleteEditAction';
+import VoteAction from '../../client-actions/vote/VoteAction';
+import DeleteEditAction from '../../client-actions/delete-edit/DeleteEditAction';
 
 const Actions = styled(CardActions)`
   display: flex;
@@ -19,12 +20,15 @@ class CardPost extends React.Component {
 
   handleExpandChange = expanded => this.setState({expanded: expanded});
 
+  handleEditPost = () => this.props.history.push(`update-post/${this.props.post.id}`);
+
   render() {
     const {expanded} = this.state;
     const {id, author, timestamp, title, commentCount, body} = this.props.post;
     const datePost = new Date(timestamp);
     const date = `${datePost.getUTCMonth() +
     1} / ${datePost.getUTCDate()} / ${datePost.getFullYear()} `;
+    const {palette} = muiTheme;
 
     return (
         <Card expanded={expanded} onExpandChange={this.handleExpandChange}>
@@ -35,19 +39,24 @@ class CardPost extends React.Component {
               showExpandableButton={true}
           />
           <CardText>
-            <h1>{title}</h1>
+            <h1 style={{color: palette.primary1Color}}>{title}</h1>
           </CardText>
           <CardTitle expandable={true}
                      subtitle={`${commentCount} comments`}
           />
-          <CardText expandable={true}>
+          <CardText style={{color: palette.primary2Color}} expandable={true}>
             {
               body.length < 200 ? body : `${body.slice(0, 200)}...`
             }
           </CardText>
           <Actions>
-            <VoteAction id={id} type='posts'/>
-            <DeleteEditAction id={id} type='posts'/>
+            <VoteAction id={id}
+                        type='posts'
+            />
+            <DeleteEditAction id={id}
+                              type='posts'
+                              handleEdit={this.handleEditPost}
+            />
             <Link to={`read-more/${id}`}>
               <FlatButton label="Read more"
                           primary={true}
@@ -63,4 +72,4 @@ CardPost.propTypes = {
   post: PropTypes.object,
 };
 
-export default CardPost;
+export default withRouter(CardPost);
