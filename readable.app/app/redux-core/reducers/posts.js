9e4eac63a9a48b9128d91afff4942cf74d4c7077
sakals <sakals@mail.ua>
2017-12-08 19:50:04 -0600

    style: formatting
    Signed-off-by: sakals <sakals@mail.ua>

---
+++ readable.app/app/redux-core/reducers/posts.js
@@ -0,0 +1,44 @@
+import actionTypes from '../actions/actionTypes';
+
+function posts(state = {}, action) {
+  const {post} = actionTypes;
+  const {type, payload = {}} = action;
+
+  const updateState = {
+    ...state,
+    [payload.id]: {
+      ...payload
+    },
+  };
+
+  switch (type) {
+    case post.ALL:
+      return {
+        ...state,
+        ...payload
+      };
+
+    case post.BY_ID:
+      return {
+        ...state,
+        [payload.id]: {
+          ...payload
+        },
+      };
+
+    case post.ADD:
+      return updateState;
+
+    case post.VOTE:
+      return updateState;
+
+    case post.DISABLE:
+      return updateState;
+
+    case post.EDIT:
+      return updateState;
+  }
+  return state;
+}
+
+export default posts;
