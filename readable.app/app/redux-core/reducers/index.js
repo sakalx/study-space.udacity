a010cc024694ca61ebedba1db605349f94e2fba2
sakals <sakals@mail.ua>
2017-12-08 19:45:49 -0600

    feat: 2 more reducers
    Signed-off-by: sakals <sakals@mail.ua>

--- readable.app/app/redux-core/reducers/index.js
+++ readable.app/app/redux-core/reducers/index.js
@@ -2,8 +2,12 @@ import {combineReducers} from 'redux';
 
 import posts from './posts';
 import categories from './categories';
+import comments from './comments';
+import snackInfo from './snackInfo';
 
 export default combineReducers({
   posts,
   categories,
+  comments,
+  snackInfo,
 });
