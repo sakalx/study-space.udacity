98b0728e15dfc75bcd86b17279158f3f664201b4
sakals <sakals@mail.ua>
2017-12-12 10:22:46 -0600

    feat: add edit delete for post-detai page
    Signed-off-by: sakals <sakals@mail.ua>

--- readable.app/app/pages/read-post/ReadMore.js
+++ readable.app/app/pages/read-post/ReadMore.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import styled from 'styled-components';
+import {withRouter} from 'react-router-dom';
 import {getPostById} from 'root/app/redux-core/actions/post';
 import {getCommByParent} from 'root/app/redux-core/actions/comment';
 
@@ -8,6 +9,7 @@ import Subheader from 'material-ui/Subheader';
 import Header from '../../header/Header';
 import LinkToHome from '../../links/LinkToHome';
 import VoteAction from '../../client-actions/vote/VoteAction';
+import DeleteEditAction from '../../client-actions/delete-edit/DeleteEditAction';
 import Paper from 'material-ui/Paper';
 import CardComment from '../../cards/comment/Comment';
 import NewComment from './scenes/NewComment';
@@ -25,6 +27,9 @@ const Comments = styled(Paper)`
   text-align: center;
   color: #757575 !important;
 `;
+const Actions = styled.div`
+  display: flex;
+`;
 
 @connect(store => ({
   posts: store.posts,
@@ -40,50 +45,63 @@ class ReadMore extends React.Component {
     this.dispatch(getCommByParent(this.id));
   };
 
+  handleEditPost = () => this.props.history.push(`/update-post/${this.id}`);
+
   render() {
-    if (!!this.props.posts[this.id]) {
-      const {id, title, body, timestamp, author} = this.props.posts[this.id];
-      const {comments} = this.props;
-      const commentsKey = Object.keys(comments);
+    const {posts} = this.props;
 
-      const sorted = commentsKey.sort((a, b) =>
-          comments[a].timestamp > comments[b].timestamp,
-      );
-
-      return (
-          <div>
-            <Header title={title}/>
-            <BackToHome>
-              <LinkToHome/>
-            </BackToHome>
-            <Author>Author: {author}</Author>
-            <p style={{whiteSpace: 'pre-line'}}>{body}</p>
-            <Subheader>{String(new Date(timestamp))}</Subheader>
-            <VoteAction id={id} type='posts'/>
-            {
-              !!sorted[0] &&
-              <Comments zDepth={3}>
-                <h2>COMMENTS</h2>
-              </Comments>
-            }
-            {
-              sorted.map(id =>
-                  !comments[id].deleted &&
-                  <CardComment
-                      key={comments[id].id}
-                      comment={comments[id]}
-                  />,
-              )
-            }
-            <br/>
-            <NewComment parentId={this.id}/>
-            <br/>
-            <SnackInfo/>
-          </div>
-      );
+    if (!posts[this.id] || posts[this.id].deleted) {
+      return <NotFound title={'Post'}/>;
     }
-    return <NotFound title={'Post'}/>;
+
+    const {id, title, body, timestamp, author} = this.props.posts[this.id];
+    const {comments} = this.props;
+    const commentsKey = Object.keys(comments);
+
+    const sorted = commentsKey.sort((a, b) =>
+        comments[a].timestamp > comments[b].timestamp,
+    );
+
+    return (
+        <div>
+          <Header title={title}/>
+          <BackToHome>
+            <LinkToHome/>
+          </BackToHome>
+          <Author>Author: {author}</Author>
+          <p style={{whiteSpace: 'pre-line'}}>{body}</p>
+          <Subheader>
+            {String(new Date(timestamp))}
+          </Subheader>
+          <Actions expandable={true}>
+            <VoteAction id={id} type='posts'/>
+            <DeleteEditAction id={id}
+                              type='posts'
+                              handleEdit={this.handleEditPost}
+            />
+          </Actions>
+          {
+            !!sorted[0] &&
+            <Comments zDepth={3}>
+              <h2>COMMENTS</h2>
+            </Comments>
+          }
+          {
+            sorted.map(id =>
+                !comments[id].deleted &&
+                <CardComment
+                    key={comments[id].id}
+                    comment={comments[id]}
+                />,
+            )
+          }
+          <br/>
+          <NewComment parentId={this.id}/>
+          <br/>
+          <SnackInfo/>
+        </div>
+    );
   }
 }
 
-export default ReadMore;
+export default withRouter(ReadMore);
